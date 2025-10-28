d5a9c366e4ba095a1da1f47d7bd210ff8d53b48c
gabrielaamaya <amayagabriela073@gmail.com>
2025-10-28 15:27:05 -0600

    se separaron en diferentes archivos el menu desplegable

---
+++ menu-despegable.js
@@ -0,0 +1,8 @@
+// Funcionalidad para los botones del menú de perfil
+document.getElementById('login-btn-dropdown').addEventListener('click', () => {
+    document.getElementById('login-btn').click();
+});
+
+document.getElementById('register-btn-dropdown').addEventListener('click', () => {
+    document.getElementById('register-btn').click();
+});
